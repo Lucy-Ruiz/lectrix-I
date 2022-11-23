c3bd1b9be17bdf7561bccfe685c35319fa6c4b8b
Lucy Ruiz <lucyruiz846@gmail.com>
2022-11-23 13:17:41 -0800

    updated book_id model from integer to charfield

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -4,5 +4,5 @@ from authentication.models import User
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=100)
-    book_id = models.IntegerField()
+    book_id = models.CharField(max_length=100)
 
