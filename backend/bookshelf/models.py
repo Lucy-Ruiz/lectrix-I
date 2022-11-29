34c9dcf618785bd073ac110037c2d006eedf1c47
Lucy Ruiz <lucyruiz846@gmail.com>
2022-11-28 21:58:55 -0800

    created bookshelf model and table in mysql

---
+++ backend/bookshelf/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+class Bookshelf(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_id = models.CharField(max_length=100)
