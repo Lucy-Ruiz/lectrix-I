e47f5cc19b76616a310cffc531356f5a91dbbbc2
Lucy Ruiz <lucyruiz846@gmail.com>
2022-12-06 15:19:55 -0800

    image, title, author and description displayed

--- frontend/src/components/BookDetails/BookDetails.jsx
+++ frontend/src/components/BookDetails/BookDetails.jsx
@@ -1,8 +1,25 @@
 const BookDetails =(props) => {
     return(
-        <thead>
-            <th>Details</th>
-        </thead>
+        <table>
+            <thead>
+                <th>Details</th>
+                    <tr>
+                        {/* <th>Image</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <th>Description</th> */}
+                    </tr>
+            </thead>
+            <tbody>
+                <img src={props.bookDetails.imageLinks.thumbnail}/>
+                {props.bookDetails.title}
+                {props.bookDetails.authors[0]}
+                {props.bookDetails.description}
+            </tbody>
+        </table>
     )
 };
 
